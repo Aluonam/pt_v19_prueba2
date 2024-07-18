e3513065ba4767d4dc8ae8f907847f95f44b0832
Paula Cano <paulacanoroman@gmail.com>
2024-07-18 14:28:13 +0200

    feat: new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 
 import './App.css'
+import UsersTable from './components/UsersTable'
 
 function App() {
 
   return (
     <>
       <div>
-       
+        <UsersTable></UsersTable>
       </div>
-
+      
     </>
   )
 }
