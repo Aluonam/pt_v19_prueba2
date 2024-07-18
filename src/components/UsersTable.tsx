4ffc201020ae8d6b9c84cb4b71540a1d2da5e85c
Paula Cano <paulacanoroman@gmail.com>
2024-07-18 14:37:15 +0200

    feat: table

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -1,7 +1,26 @@
 
 const UsersTable = () => {
   return (
-    <div>UsersTable</div>
+    <>
+        <table  style={{backgroundColor:'aliceblue', border:'1px solid black'}}>
+            <thead >
+                <tr style={{backgroundColor:'aliceblue', border:'1px solid black'}}>
+                    <td>Name</td>
+                    <td>Gender</td>
+                    <td>Location</td>
+                    <td>Email</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td>Gender</td>
+                    <td>Location</td>
+                    <td>Email</td>
+                </tr>
+            </tbody>
+        </table>
+    </>
   )
 }
 
