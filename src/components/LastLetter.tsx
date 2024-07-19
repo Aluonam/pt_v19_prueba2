d1fa67ae1a09dbd505ae8c0a33fe43d8668848df
Paula Cano <paulacanoroman@gmail.com>
2024-07-19 09:45:58 +0200

    feat: capture user sentence

--- src/components/LastLetter.tsx
+++ src/components/LastLetter.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react"
 
 
 const LastLetter = () => {
+
+    const [userSentence, setUserSentence] = useState<string>('')
   return (
     <div>
-        <input placeholder="Escribe una frase"></input>
+        <input placeholder="Escribe una frase" onChange={(e)=>{setUserSentence(e.target.value)}}></input>
         <button>Última letra en mayúscula</button>
         <br></br>
+        {userSentence}
     </div>
   )
 }
